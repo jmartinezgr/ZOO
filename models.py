80614fda6d9a6833496ccfc23bb822a5ebb76578
Juan Jose Martinez <josemargri3@gmail.com>
2023-05-17 14:20:21 -0500

    models.py file created and the methos of the class zoo are defined

---
+++ models.py
@@ -0,0 +1,20 @@
+class Zoologico:
+    def __init__(self):
+        self.habitats = []
+        self.animales = []
+        self.url = []
+
+    def cargarInfo(self):
+        pass
+    
+    def agregar_animal(self):
+        pass
+
+    def agregar_habitat(self):
+        pass
+
+    def mostrar_animales(self):
+        pass
+
+    def mostrar_habitats(self):
+        pass
